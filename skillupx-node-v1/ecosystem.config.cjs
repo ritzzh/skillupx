0abad71ccd02a7bdcc0af5aa3f57a6d755dfdbbf
Ritzzh <ritesh.singh.19a@gmail.com>
2025-12-07 00:42:59 +0530

    Ritzzh | Dec 7 | Setting up Deployment

--- skillupx-node-v1/ecosystem.config.cjs
+++ skillupx-node-v1/ecosystem.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     production : {
       user : 'root',
       host : '31.97.233.174',
-      ref  : 'origin/main',
+      ref  : 'origin/master',
       repo : 'git@github.com:ritzzh/skillupx.git',
       path : '/var/www/node-backend',
       'pre-deploy-local': '',
